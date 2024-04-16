2efaf9791bb383a3829fa9453837befbfb6f8252
vdelri0 <victordelrio333@gmail.com>
2024-04-16 15:54:56 -0500

    Creates and modifies Meeting and Room models and adds them to the admin app.

--- meeting_planner/meetings/models.py
+++ meeting_planner/meetings/models.py
@@ -1,6 +1,23 @@
+from datetime import time
 from django.db import models
 
 # Create your models here.
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    floor = models.IntegerField()
+    room_number = models.IntegerField()
+
+    def __str__(self) -> str:
+        return f"{self.name}: room {self.room_number} on floor {self.floor}"
+
 class Meeting(models.Model):
     title = models.CharField(max_length=200)
     date = models.DateField()
+    start_time = models.TimeField(default=time(9))
+    duration = models.IntegerField(default=1)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+
+    # As you're not modifying model fields, you can add behavior without migrate.
+    def __str__(self) -> str:
+        return f"{self.title} at {self.start_time} on {self.date}"
+
