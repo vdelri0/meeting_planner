2e17a5c26e0881e41193ea80297ef5b806c2816f
vdelri0 <victordelrio333@gmail.com>
2024-04-16 13:25:03 -0500

    Added date and about views.

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,6 +1,13 @@
+from datetime import datetime
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 def welcome(request):
     return HttpResponse("Welcome to the Meeting Plannner!")
+
+def date(request):
+    return HttpResponse("This page was served at " + str(datetime.now()))
+
+def about(request):
+    return HttpResponse("Hi I'm Victor Del Rio and I implement ideas into robust projects!")
