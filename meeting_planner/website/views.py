bb5f635480bb87e27dee6046ee4dcd81ce04b8d9
vdelri0 <victordelrio333@gmail.com>
2024-05-07 10:48:51 -0500

    Adds num_meetings implementing MTV from begining to end.

--- meeting_planner/website/views.py
+++ meeting_planner/website/views.py
@@ -1,11 +1,14 @@
 from datetime import datetime
+
 from django.shortcuts import render
 from django.http import HttpResponse
+
+from meetings.models import Meeting
 
 # Create your views here.
 def welcome(request):
     return render(request, "website/welcome.html",
-                  {"message": "This data was sent from the view to the template."})
+                  {"num_meetings": Meeting.objects.count()})
 
 def date(request):
     return HttpResponse("This page was served at " + str(datetime.now()))
